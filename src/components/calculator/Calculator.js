5b9c2d07596ec7dc3545323ed8baec4c1571d63c
od-c0d3r <omar_rashad@aol.com>
2022-01-05 14:39:07 +0200

    Code: convert App to be class compenent

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,29 +1,14 @@
-import logo from '../../imgs/logo.svg';
-import './App.css';
+import './Calculator.scss';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>This is the Calculator Class Component</p>;
+  }
 }
 
-export default App;
+export default Calculator;
