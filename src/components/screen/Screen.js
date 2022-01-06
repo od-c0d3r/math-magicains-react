803aba234978e41c9455a8ce5e6c3577deeea0dc
od-c0d3r <omar_rashad@aol.com>
2022-01-06 22:17:39 +0200

    Code: refactor Screen component

--- src/components/screen/Screen.js
+++ src/components/screen/Screen.js
@@ -3,19 +3,13 @@ import './Screen.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Screen extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { screen } = this.props;
-    return (
-      <div id="calScreen">
-        {screen}
-      </div>
-    );
-  }
+const Screen = (props) => {
+  const { screen } = props;
+  return (
+    <div id="calScreen">
+      {screen}
+    </div>
+  )
 }
 
 Screen.propTypes = {
