92576a4b0320dd17119f1abf10dcdf4c962fcd3b
od-c0d3r <omar_rashad@aol.com>
2022-01-12 11:06:41 +0200

    Feat: construct Navbar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,19 @@
 import './App.scss';
 import 'normalize.css';
 import React from 'react';
+import { Route, Router, Navigate } from 'react-router-dom';
 import Header from '../header/Header';
 import CalculatorPage from '../calculatorPage/calculatorPage';
 
 const App = () => (
   <div id="App" style={{ height: window.innerHeight }}>
     <Header />
-    <CalculatorPage />
+    <Router className="App-intro">
+      <Route path="/home" exact component={HomePage} />
+      <Route path="/calc" component={CalculatorPage} />
+      <Route path="/quote" component={QuotePage} />
+      <Navigate to="/home" />
+    </Router>
   </div>
 );
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,13 @@
 import './Navbar.scss';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
   <nav id="navbar">
-    #navbar
+    <ul>
+      <li><NavLink to="/home">Home</NavLink></li>
+      <li><NavLink to="/calc">Calculator</NavLink></li>
+      <li><NavLink to="/quote">Quote</NavLink></li>
+    </ul>
   </nav>
 );
 
